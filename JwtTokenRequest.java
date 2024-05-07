cd29c764887e90c8bf80e979394e5cadc0be8d87
balram4455 <161879990+balram4455@users.noreply.github.com>
2024-05-07 18:32:51 +0530

    Add files via upload

---
+++ JwtTokenRequest.java
@@ -0,0 +1,5 @@
+package com.balram.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
+
+
---
+++ JwtTokenResponse.java
@@ -0,0 +1,5 @@
+package com.balram.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
